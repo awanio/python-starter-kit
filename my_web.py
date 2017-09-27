82bd920de3e8d42c40d9e789da87b72bee0b1e6e
Iskandar Soesman <k4ndar@yahoo.com>
2017-09-28 06:35:15 +0700

    Fixing data type for wsgi

--- my_web.py
+++ my_web.py
@@ -7,4 +7,4 @@ def app(environ, start_response):
         ('Content-Length', str(len(data)))
     ]
     start_response(status, response_headers)
-    return iter([data])
+    return [bytes(data, 'utf-8')]
